77e9370ba876c3b3d9d606d36d79a5938cf46e3e
meetul <meetulrathore98@gmail.com>
2023-02-02 13:58:52 +0530

    add image upload

--- seeds/index.js
+++ seeds/index.js
@@ -22,13 +22,22 @@ const seedDB = async () => {
     await Campground.deleteMany();
     for (let i = 0; i < 50; ++i) {
         const rand1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random()*20) + 10;
+        const price = Math.floor(Math.random() * 20) + 10;
 
         const camp = new Campground({
             author: "63d37b4103c9b797d336b52d",
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: "https://source.unsplash.com/collection/483251",
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dmikuw8zs/image/upload/v1675254143/YelpCamp/fkonzzmnsk3xxhudwmit.jpg',
+                    filename: 'YelpCamp/fkonzzmnsk3xxhudwmit'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dmikuw8zs/image/upload/v1675254145/YelpCamp/n9sodaj24qcnomnzhjts.jpg',
+                    filename: 'YelpCamp/n9sodaj24qcnomnzhjts'
+                }
+            ],
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus doloribus amet porro molestiae in ipsam, a beatae odio nesciunt illo dolore perspiciatis. Enim reprehenderit voluptatem in magni soluta voluptate quibusdam.",
             price
         })
